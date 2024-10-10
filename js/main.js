e3a07315a8fd6066516a365d1a05024420789d32
ahmed <ahmed@localhost.localdomain>
2024-10-10 20:49:42 +0300

    Add Logic To Detremine The Winner

--- js/main.js
+++ js/main.js
@@ -3,15 +3,18 @@ let firstGame = true;
 
 function updateValue(event) {
   const square = event.target;
+  let winner;
 
   if (square.classList.contains('square')) {
     const isSquareEmpty = /^\s*$/gi.test(square.textContent);
     if(isSquareEmpty) {
       const playerValue = currentPlayer();
-      square.textContent = playerValue;
       square.setAttribute('data-value', playerValue);
+      winner = checkWinner(playerValue);
+      console.log(winner);
+      square.textContent = playerValue;
     }
-    displayInfo();
+    displayInfo(winner);
   }
 }
 
@@ -23,19 +26,28 @@ function currentPlayer() {
   return player;
 }
 
-// Function TO Display Information About The Current Game
-function displayInfo() {
+// Function To Display Information About The Current Game
+function displayInfo(winner) {
   let swapCurrentPlayer = (player === 'O' ? 'X' : 'O');
   nextPlayer = swapCurrentPlayer;
   const h2 = document.querySelector('.tic-tac-toe h2.info');
-  h2.textContent = `${firstGame ? 'First Game:' : (checkWinner() ? 'Winner:' : 'Next Player:')} ${firstGame ? player : nextPlayer}`;
+  h2.textContent = `${firstGame ? 'First Game:' : winner ? 'Winner:' : 'Next Player:'} ${firstGame || winner ? player : nextPlayer }`
 }
 displayInfo();
 
 // Determine who Is The Winner
-function checkWinner() {
-const winList = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
-  return false;
+function checkWinner(currentPlayer) {
+  const winList = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+
+  const winner = winList.some((win) => {
+
+    return win.every((square) => {
+      return currentPlayer === document.getElementById(square).dataset.value
+    });
+
+  })
+
+  return winner;
 }
 
 const game = document.querySelector('.tic-tac-toe .squares');
